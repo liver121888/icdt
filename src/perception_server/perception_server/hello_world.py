03f23247b7d5d731073d8509f8783183701131e9
warra <warrier.abhishek@gmail.com>
2024-10-30 15:53:54 -0400

    feat: create default package for perception server

---
+++ src/perception_server/perception_server/hello_world.py
@@ -0,0 +1,6 @@
+def main():
+    print('Hi from perception_server.')
+
+
+if __name__ == '__main__':
+    main()
---
+++ src/perception_server/setup.py
@@ -0,0 +1,26 @@
+from setuptools import find_packages, setup
+
+package_name = 'perception_server'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='warra',
+    maintainer_email='warrier.abhishek@gmail.com',
+    description='Node to advertise detection services',
+    license='MIT',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'hello_world = perception_server.hello_world:main'
+        ],
+    },
+)
